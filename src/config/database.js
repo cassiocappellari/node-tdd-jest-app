44a01890f4ddd732a749d19e067875d5134a5dc3
Cassio Cappellari <cassiocappellari@gmail.com>
2020-12-23 08:58:21 -0300

    criado o arquivo .sequelizerc para configurar o caminho das pastas de database, models, seeders e migrations; arquivo server.js atualizado com as portas; arquivo de rotas criado; arquivo de configuracao de banco de dados criado e atualizado

---
+++ src/config/database.js
@@ -0,0 +1,14 @@
+module.exports = {
+  host: '127.0.0.1',
+  username: 'postgres',
+  password: 'postgres',
+  database: 'node-tdd-jest-app',
+  dialect: 'postgres',
+  operatorsAliases: false,
+  logging: false,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true
+  }
+}
---
+++ src/routes.js
@@ -0,0 +1,5 @@
+const routes = require('express').Router()
+
+// Definição das rotas da aplicação
+
+module.exports = routes

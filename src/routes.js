fc2259fec646d028b17f6a9b62268da8ffd0012a
Cassio Cappellari <cassiocappellari@gmail.com>
2020-12-26 12:38:24 -0300

    biblioteca jest instalada e configurada

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,3 @@
 const routes = require('express').Router()
-const {User} = require('./app/models')
-
-User.create({
-    name: "Cássio",
-    email: "cassiocappellari@gmail.com",
-    password_hash: "123456789"
-})
 
 module.exports = routes

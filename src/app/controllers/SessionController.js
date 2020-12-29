c958e8b4296c9f528cacbffa290232d983060065
Cassio Cappellari <cassiocappellari@gmail.com>
2020-12-29 11:59:23 -0300

    teste de autenticacao atualizado; controller atualizado com verificacao de password; model de usuario atualizado com verificacao de senha entre req.body.password e senha armazenada no banco de dados

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,6 +1,20 @@
+const {User} = require('../models')
+
 class SessionController {
     async store(req, res) {
-        return res.status(200).send()
+        const {email, password} = req.body
+
+        const user = await User.findOne({where: {email}})
+
+        if (!user) {
+            return res.status(401).json({message: 'User not found'})
+        }
+
+        if (!(await user.checkPassword(password))) {
+            return res.status(401).json({message: 'Invalid password'})
+        }
+
+        return res.json({user})
     }
 }
 
--- tests/integration/session.test.js
+++ tests/integration/session.test.js
@@ -19,9 +19,26 @@ describe('Authentication', () => {
             .post('/sessions')
             .send({
                 email: user.email,
-                password: '12346'
+                password: '123123'
             })
 
         expect(response.status).toBe(200)
+    })
+
+    it('should not authenticate with invalid credentials', async () => {
+        const user = await User.create({
+            name: 'Cássio',
+            email: 'cassiocappellari@gmail.com',
+            password: '123123'
+        })
+
+        const response = await request(app)
+            .post('/sessions')
+            .send({
+                email: user.email,
+                password: '123789'
+            })
+
+        expect(response.status).toBe(401)
     })
 })

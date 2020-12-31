288b0993f29ba96861e97650345be49a2e1d2f97
Cassio Cappellari <cassiocappellari@gmail.com>
2020-12-31 12:44:35 -0300

    teste de integracao atualizado com verificacao da validade do token; middleware de autenticacao atualizado com validacao de token

--- src/app/middleware/auth.js
+++ src/app/middleware/auth.js
@@ -1,11 +1,23 @@
 const jwt = require('jsonwebtoken')
+const {promisify} = require('util')
 
-module.exports = (req, res, next) => {
+module.exports = async (req, res, next) => {
    const authHeader = req.headers.authorization
    
    if (!authHeader) {
-       return res.status(401).json({message: 'Token not provided'})
+        return res.status(401).json({message: 'Token not provided'})
    }
 
-   return next()
+   const[, token] = authHeader.split(' ')
+
+   try {
+        const decoded = await promisify(jwt.verify)(token, process.env.APP_SECRET)
+
+        req.userId = decoded.id
+
+        return next()
+   } catch (err) {
+        return res.status(401).json({message: 'Invalid token'})
+   }
+
 }
--- tests/integration/session.test.js
+++ tests/integration/session.test.js
@@ -70,4 +70,12 @@ describe('Authentication', () => {
 
         expect(response.status).toBe(401)
     })
+
+    it('should not be able to access private routes with invalid jwt token', async () => {
+        const response = await request(app)
+            .get('/dashboard')
+            .set('Authorization', `Bearer 123123`)
+
+        expect(response.status).toBe(401)
+    })
 })

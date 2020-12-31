10ed338d6f4057279f0d7e9fe6481b4122913e4d
Cassio Cappellari <cassiocappellari@gmail.com>
2020-12-31 10:59:41 -0300

    biblioteca faker instalada e configurada no arquivo factories.js

--- tests/utils/factories.js
+++ tests/utils/factories.js
@@ -1,10 +1,11 @@
+const faker = require('faker')
 const {factory} = require('factory-girl')
 const {User} = require('../../src/app/models')
 
 factory.define('User', User, {
-    name: 'Cássio',
-    email: 'cassiocappellari@gmail.com',
-    password: '123456'
+    name: faker.name.findName(),
+    email: faker.internet.email(),
+    password: faker.internet.password()
 })
 
 module.exports = factory
